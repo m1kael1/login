c5b98d94d3d3bdf9b0a0895a16847f8c6ea0fe48
Kelyna <94010783+m1kael1@users.noreply.github.com>
2022-12-04 18:47:27 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
+const clearUsername = document.getElementsByClassName("input");
+
 function closeForm() {
-	document.querySelector(".blur").style.display = "none";
+	document.getElementById("logsign").style.display = "none";
+	for (let i = 0; i < 5; i++) {
+		clearUsername[i].value = "";
+	}
 }
+
 function showFormLogin() {
-	document.querySelector(".blur").style.display = "flex";
+	document.getElementById("logsign").style.display = "flex";
+	document.querySelector("#login").style.display = "flex";
 	document.querySelector("#login").style.display = "flex";
 	document.querySelector("#sign-up").style.display = "none";
 }
 function showFormSignup() {
-	document.querySelector(".blur").style.display = "flex";
+	document.getElementById("logsign").style.display = "flex";
 	document.querySelector("#sign-up").style.display = "flex";
 	document.querySelector("#login").style.display = "none";
+}
+
+// LOGIN
+function myLogin() {
+	var user = document.forms["inputuserform"]["uname"].value;
+	// var pw = document.forms["inputuserform"]["psw"].value;
+	if (user == user) {
+		document.write("<html><body><h1><center>");
+		document.write("HI." + " ");
+		document.write(user);
+		document.write("</center></h1></body></html");
+	}
 }
